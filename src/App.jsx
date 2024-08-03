eb4313676f2a28a09e11b281195457fccd554de2
James Janidas <87832509+JamesJNeu@users.noreply.github.com>
2024-08-02 17:09:32 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "James Janidas",
+  title: "Senior Developer",
+  email: "james.janidas@neudesic.com",
+  gitHub: "JamesJNeu",
+  instagram: "",
+  linkedIn: "jamesjanidas",
   medium: "",
-  twitter: "microsoft",
+  twitter: "",
   youTube: "Code",
 };
 
